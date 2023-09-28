0cf7e22ce2c509ababddb1fb57ac37b41c451ba9
Ayaan Grover <ayaangrover@gmail.com>
2023-09-28 14:00:38 -0700

    swdefrt5y67u8i9

--- script.js
+++ script.js
@@ -1,63 +1,80 @@
-const list = document.getElementsByClassName("list")[0];
-const newReminder = document.getElementById("newReminder");
+document.addEventListener("DOMContentLoaded", function () {
+    const reminderForm = document.getElementById("reminder-form");
+    const reminderList = document.getElementById("reminder-list");
 
-// Create reminder function (Template Card)
-function createReminder(id, message) {
-    // Error handling
-    if (message && message.length > 45) {
-        alert("We only support 45 characters");
-        return;
-    } else if (!message) {
-        alert("Please enter a Reminder");
-        return;
+    // Load reminders from local storage when the page loads
+    const savedReminders = JSON.parse(localStorage.getItem("reminders")) || [];
+
+    function saveRemindersToLocalStorage() {
+        localStorage.setItem("reminders", JSON.stringify(savedReminders));
     }
 
-    const li = document.createElement("li");
-    li.id = id;
-    li.className = "reminder";
-    const div = document.createElement("div");
-    div.className = "text";
-    div.innerText = message;
+    // Function to add a reminder to the list and save it to local storage
+    function addReminderToList(title, date, description) {
+        const reminderItem = document.createElement("li");
+        reminderItem.innerHTML = `
+            <div class="reminder-content">
+                <h3>${title}</h3>
+                <p><strong>Date:</strong> ${date}</p>
+                ${description ? `<p><strong>Description:</strong> ${description}</p>` : ''}
+            </div>
+            <div class="reminder-actions">
+                <button class="complete-button">Completed</button>
+                <button class="remove-button">Remove</button>
+            </div>
+        `;
 
-    // Actions container
-    const actionContainer = document.createElement("div");
-    actionContainer.className = "actions"
+        reminderList.appendChild(reminderItem);
 
-    // Have the check and delete buttons
-    const checkBtn = document.createElement("button");
-    checkBtn.className = "btn-check";
-    checkBtn.innerText = "Checked";
+        // Add event listeners for hover actions
+        reminderItem.addEventListener("mouseenter", function () {
+            const actions = reminderItem.querySelector(".reminder-actions");
+            actions.style.display = "block";
+        });
 
-    const deleteBtn = document.createElement("button");
-    deleteBtn.className = "btn-delete";
-    deleteBtn.innerText = "Delete";
+        reminderItem.addEventListener("mouseleave", function () {
+            const actions = reminderItem.querySelector(".reminder-actions");
+            actions.style.display = "none";
+        });
 
-    // Events handler functions
-    checkBtn.addEventListener("click", function () {
-        if (li.id == id) {
-            div.style.textDecoration = "line-through";
-        }
+        // Add event listeners for completing and removing reminders
+        const completeButton = reminderItem.querySelector(".complete-button");
+        const removeButton = reminderItem.querySelector(".remove-button");
+
+        completeButton.addEventListener("click", function () {
+            // Handle completing the reminder (you can add your logic here)
+            reminderItem.style.textDecoration = "line-through";
+            saveRemindersToLocalStorage();
+        });
+
+        removeButton.addEventListener("click", function () {
+            // Handle removing the reminder
+            reminderList.removeChild(reminderItem);
+            savedReminders.splice(savedReminders.indexOf(title), 1); // Remove from saved reminders
+            saveRemindersToLocalStorage();
+        });
+
+        // Save the reminder to local storage
+        savedReminders.push({ title, date, description });
+        saveRemindersToLocalStorage();
+    }
+
+    // Load saved reminders from local storage and display them
+    for (const reminder of savedReminders) {
+        addReminderToList(reminder.title, reminder.date, reminder.description);
+    }
+
+    reminderForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        const title = document.getElementById("title").value;
+        const date = document.getElementById("date").value;
+        const description = document.getElementById("description").value;
+
+        // Add the reminder to the list and save it to local storage
+        addReminderToList(title, date, description);
+
+        // Clear the form fields
+        reminderForm.reset();
     });
-
-    deleteBtn.addEventListener("click", function () {
-        if (li.id == id) {
-            list.removeChild(li);
-        }
-    });
-
-    // Append the button for actions
-    actionContainer.appendChild(checkBtn);
-    actionContainer.appendChild(deleteBtn);
-    // Append all the elements in Li
-    li.appendChild(div);
-    li.appendChild(actionContainer);
-
-    return li;
-}
-
-newReminder.addEventListener("click", function () {
-    let message = prompt("Please enter a Reminder");
-    let id = Math.floor(Math.random() * 100);
-    let reminder = createReminder(id, message);
-    list.appendChild(reminder);
 });
